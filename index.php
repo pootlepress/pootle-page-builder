6e33c0dad6b07e735da6a1963ad2237d88f0c346
Shramee Srivastav <shramee.srivastav@gmail.com>
2015-11-05 21:32:02 +0530

    pootle page builder add-ons rss

---
+++ index.php
@@ -0,0 +1,142 @@
+<?xml version="1.0" encoding="UTF-8"?><rss version="2.0"
+                                           xmlns:content="http://purl.org/rss/1.0/modules/content/"
+                                           xmlns:wfw="http://wellformedweb.org/CommentAPI/"
+                                           xmlns:dc="http://purl.org/dc/elements/1.1/"
+                                           xmlns:atom="http://www.w3.org/2005/Atom"
+                                           xmlns:sy="http://purl.org/rss/1.0/modules/syndication/"
+                                           xmlns:slash="http://purl.org/rss/1.0/modules/slash/"
+	>
+
+	<channel>
+		<title>pootlepress &#187; Product Categories &#187; pootle page builder add-ons</title>
+		<atom:link href="http://www.pootlepress.com/feed/?product_cat=pootle-page-builder-add-ons" rel="self" type="application/rss+xml" />
+		<link>http://www.pootlepress.com</link>
+		<description></description>
+		<lastBuildDate>Thu, 05 Nov 2015 14:48:32 +0000</lastBuildDate>
+		<language>en-US</language>
+		<sy:updatePeriod>hourly</sy:updatePeriod>
+		<sy:updateFrequency>1</sy:updateFrequency>
+		<generator>http://wordpress.org/?v=4.3.1</generator>
+		<item>
+			<title>Blog customizer for pootle page builder</title>
+			<link>http://www.pootlepress.com/shop/pootle-page-builder-blog-customizer/</link>
+			<comments>http://www.pootlepress.com/shop/pootle-page-builder-blog-customizer/#comments</comments>
+			<pubDate>Thu, 01 Oct 2015 09:43:42 +0000</pubDate>
+			<dc:creator><![CDATA[Nick Burne]]></dc:creator>
+
+			<guid isPermaLink="false">http://www.pootlepress.com/?post_type=product&#038;p=22087</guid>
+			<description><![CDATA[<p>Post Customizer allows you to pull in posts to create stunning pages with beautiful post layouts. Watch the video to see how amazing it really is.</p>
+				<p>The post <a rel="nofollow" href="http://www.pootlepress.com/shop/pootle-page-builder-blog-customizer/">Blog customizer for pootle page builder</a> appeared first on <a rel="nofollow" href="http://www.pootlepress.com">pootlepress</a>.</p>
+				]]></description>
+			<content:encoded><![CDATA[<p>Post Customizer allows you to pull in posts to create stunning pages with beautiful post layouts. Watch the video to see how amazing it really is.</p>
+				<h2>Features</h2>
+				<p>With pootle page builder post customizer you can:</p>
+				<ul>
+					<li>Create stunning pages featuring your posts with different layouts and options</li>
+					<li>Four beautiful layout options and more coming soon</li>
+					<li>Filter posts by category</li>
+					<li>Add post meta above title, below title or below excerpt</li>
+				</ul>
+				<h2>Support and upgrades</h2>
+				<ul>
+					<li>Premium support provided for 1 year based on the number of sites licensed</li>
+					<li>Free updates provided for 1 year for based on the number of sites licensed</li>
+					<li>Update within WordPress with a notification or download from your pootlepress store account</li>
+				</ul>
+				<h2>Walk-through Video</h2>
+				<p><iframe src="https://player.vimeo.com/video/140391621?title=0&amp;byline=0&amp;portrait=0" width="800" height="450" frameborder="0" allowfullscreen="allowfullscreen"></iframe></p>
+				<p>The post <a rel="nofollow" href="http://www.pootlepress.com/shop/pootle-page-builder-blog-customizer/">Blog customizer for pootle page builder</a> appeared first on <a rel="nofollow" href="http://www.pootlepress.com">pootlepress</a>.</p>
+				]]></content:encoded>
+			<wfw:commentRss>http://www.pootlepress.com/shop/pootle-page-builder-blog-customizer/feed/</wfw:commentRss>
+			<slash:comments>0</slash:comments>
+		</item>
+		<item>
+			<title>Page customizer for pootle page builder</title>
+			<link>http://www.pootlepress.com/shop/page-customizer-for-pootle-page-builder/</link>
+			<comments>http://www.pootlepress.com/shop/page-customizer-for-pootle-page-builder/#comments</comments>
+			<pubDate>Sun, 04 Oct 2015 18:19:31 +0000</pubDate>
+			<dc:creator><![CDATA[Jamie Marsland]]></dc:creator>
+
+			<guid isPermaLink="false">http://www.pootlepress.com/?post_type=product&#038;p=22693</guid>
+			<description><![CDATA[<p>Page customizer gives you lots of controls to customize individual WordPress pages, including, Add fullscreen background image per page Add fullscreen background video per page Change header colour per page Change header background image per page Change footer colour per page Hide page title, header, breadcrumbs, sidebars and footer per page Change mobile background images [&#8230;]</p>
+				<p>The post <a rel="nofollow" href="http://www.pootlepress.com/shop/page-customizer-for-pootle-page-builder/">Page customizer for pootle page builder</a> appeared first on <a rel="nofollow" href="http://www.pootlepress.com">pootlepress</a>.</p>
+				]]></description>
+			<content:encoded><![CDATA[<h2>Video walkthrough</h2>
+				<p><iframe src="https://player.vimeo.com/video/141336478" width="500" height="281" frameborder="0" title="Page Customizer sneak peek" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe></p>
+				<p>&nbsp;</p>
+				<p>The post <a rel="nofollow" href="http://www.pootlepress.com/shop/page-customizer-for-pootle-page-builder/">Page customizer for pootle page builder</a> appeared first on <a rel="nofollow" href="http://www.pootlepress.com">pootlepress</a>.</p>
+				]]></content:encoded>
+			<wfw:commentRss>http://www.pootlepress.com/shop/page-customizer-for-pootle-page-builder/feed/</wfw:commentRss>
+			<slash:comments>0</slash:comments>
+		</item>
+		<item>
+			<title>The One Pager for pootle page builder</title>
+			<link>http://www.pootlepress.com/shop/the-one-pager/</link>
+			<comments>http://www.pootlepress.com/shop/the-one-pager/#comments</comments>
+			<pubDate>Thu, 01 Oct 2015 11:01:53 +0000</pubDate>
+			<dc:creator><![CDATA[Jamie Marsland]]></dc:creator>
+
+			<guid isPermaLink="false">http://www.pootlepress.com/?post_type=product&#038;p=22612</guid>
+			<description><![CDATA[<p>The One Pager add on for pootle page builder makes creating long smooth scrolling websites super easy</p>
+				<p>The post <a rel="nofollow" href="http://www.pootlepress.com/shop/the-one-pager/">The One Pager for pootle page builder</a> appeared first on <a rel="nofollow" href="http://www.pootlepress.com">pootlepress</a>.</p>
+				]]></description>
+			<content:encoded><![CDATA[<p>The One Pager for pootle page builder makes it easy and quick to create beautiful long scrolling wordpress pages</p>
+				<h2>Features</h2>
+				<p>With The One Pager for pootle page builder you can</p>
+				<ul>
+					<li>Create smooth scrolling pages</li>
+					<li>Navigation icons get added automatically</li>
+					<li>Menu items are automatically added for you to use</li>
+					<li>No coding needed</li>
+				</ul>
+				<h2>Support and upgrades</h2>
+				<ul>
+					<li>Premium support provided for 1 year based on the number of sites licensed</li>
+					<li>Free updates provided for 1 year for based on the number of sites licensed</li>
+					<li>Update within WordPress with a notification or download from your PootlePress store account</li>
+				</ul>
+				<h2>Walk-through Video</h2>
+				<p><iframe src="https://player.vimeo.com/video/141027418?title=0&amp;byline=0&amp;portrait=0" width="600" height="337" frameborder="0" allowfullscreen="allowfullscreen"></iframe></p>
+				<p>&nbsp;</p>
+				<p>The post <a rel="nofollow" href="http://www.pootlepress.com/shop/the-one-pager/">The One Pager for pootle page builder</a> appeared first on <a rel="nofollow" href="http://www.pootlepress.com">pootlepress</a>.</p>
+				]]></content:encoded>
+			<wfw:commentRss>http://www.pootlepress.com/shop/the-one-pager/feed/</wfw:commentRss>
+			<slash:comments>0</slash:comments>
+		</item>
+		<item>
+			<title>WooCommerce for pootle page builder</title>
+			<link>http://www.pootlepress.com/shop/woocommerce-addon-for-pootle-page-builder/</link>
+			<comments>http://www.pootlepress.com/shop/woocommerce-addon-for-pootle-page-builder/#comments</comments>
+			<pubDate>Wed, 19 Aug 2015 13:27:30 +0000</pubDate>
+			<dc:creator><![CDATA[Nick Burne]]></dc:creator>
+
+			<guid isPermaLink="false">http://www.pootlepress.com/?post_type=product&#038;p=21895</guid>
+			<description><![CDATA[<p>pootle page builder for WooCommerce brings powerful WooCommerce features into page builder. With pootle page builder for WooCommerce you can create stunning pages featuring your products by id, category, attribute, best-selling, top rated and on-sale &#8211; plus you can use pootle page builder on product pages and with WooCommerce Tab Manager.</p>
+				<p>The post <a rel="nofollow" href="http://www.pootlepress.com/shop/woocommerce-addon-for-pootle-page-builder/">WooCommerce for pootle page builder</a> appeared first on <a rel="nofollow" href="http://www.pootlepress.com">pootlepress</a>.</p>
+				]]></description>
+			<content:encoded><![CDATA[<p>pootle page builder for WooCommerce brings powerful WooCommerce features into page builder.</p>
+				<h2>Features</h2>
+				<p>With pootle page builder for WooCommerce you can:</p>
+				<ul>
+					<li>Create stunning pages featuring your products by id, category, attribute, best-selling, top rated and on-sale</li>
+					<li>Use pootle page builder on product pages</li>
+					<li>Use pootle page builder with WooCommerce Tab Manager</li>
+				</ul>
+				<h2>Support and upgrades</h2>
+				<ul>
+					<li>Premium support provided for 1 year based on the number of sites licensed</li>
+					<li>Free updates provided for 1 year for based on the number of sites licensed</li>
+					<li>Update within WordPress with a notification or download from your PootlePress store account</li>
+				</ul>
+				<h2>Walk-through Videos</h2>
+				<h3>1. Creating stunning pages using different combinations of products</h3>
+				<p><iframe src="https://player.vimeo.com/video/137234634?title=0&amp;byline=0&amp;portrait=0" width="600" height="338" frameborder="0" allowfullscreen="allowfullscreen"></iframe></p>
+				<h3>2. Using pootle page builder for WooCommerce with product pages &amp; WooCommerce Tab Manager</h3>
+				<p><iframe src="https://player.vimeo.com/video/137235173?title=0&amp;byline=0&amp;portrait=0" width="600" height="338" frameborder="0" allowfullscreen="allowfullscreen"></iframe></p>
+				<p>The post <a rel="nofollow" href="http://www.pootlepress.com/shop/woocommerce-addon-for-pootle-page-builder/">WooCommerce for pootle page builder</a> appeared first on <a rel="nofollow" href="http://www.pootlepress.com">pootlepress</a>.</p>
+				]]></content:encoded>
+			<wfw:commentRss>http://www.pootlepress.com/shop/woocommerce-addon-for-pootle-page-builder/feed/</wfw:commentRss>
+			<slash:comments>0</slash:comments>
+		</item>
+	</channel>
+</rss>
