77f25733e91c24e36c026f8f871c5919f301e631
shramee <shramee.srivastav@gmail.com>
2016-02-04 21:19:34 +0530

    Addressing pootlepress/ppb-live-editor#43 Error

--- js/ragadjust.js
+++ js/ragadjust.js
@@ -3,50 +3,48 @@ ragadjust = function (s, method) {
 	if (document.querySelectorAll) {
 
 		var eles = document.querySelectorAll(s),
-				elescount = eles.length,
+			elescount = eles.length,
 
-				preps = /(\s|^|>)((aboard|about|above|across|after|against|along|amid|among|anti|around|before|behind|below|beneath|beside|besides|between|beyond|concerning|considering|despite|down|during|except|excepting|excluding|following|from|inside|into|like|minus|near|onto|opposite|outside|over|past|plus|regarding|round|save|since|than|that|this|through|toward|towards|under|underneath|unlike|until|upon|versus|with|within|without)?\s)+/gi,
+			preps = /(\s|^|>)((aboard|about|above|across|after|against|along|amid|among|anti|around|before|behind|below|beneath|beside|besides|between|beyond|concerning|considering|despite|down|during|except|excepting|excluding|following|from|inside|into|like|minus|near|onto|opposite|outside|over|past|plus|regarding|round|save|since|than|that|this|through|toward|towards|under|underneath|unlike|until|upon|versus|with|within|without)?\s)+/gi,
 
-				smallwords = /(\s|^)(([a-zA-Z-_(]{1,2}('|’)*[a-zA-Z-_,;]{0,1}?\s)+)/gi, // words with 3 or less characters
+			smallwords = /(\s|^)(([a-zA-Z-_(]{1,2}('|’)*[a-zA-Z-_,;]{0,1}?\s)+)/gi, // words with 3 or less characters
 
-				dashes = /([-–—])\s/gi,
+			dashes = /([-–—])\s/gi,
 
-				wspace = skipWhitespace ? /^\s$/ : /\s/gi,
-
-				emphasis = /(<(strong|em|b|i)>)(([^\s]+\s*){2,3})?(<\/(strong|em|b|i)>)/gi;
+			emphasis = /(<(strong|em|b|i)>)(([^\s]+\s*){2,3})?(<\/(strong|em|b|i)>)/gi;
 
 		while (elescount-- > 0) {
 
 			var ele = eles[elescount],
-					elehtml = ele.innerHTML;
+				elehtml = ele.innerHTML;
 
 			if (method == 'prepositions' || method == 'all')
 
-				// replace prepositions (greater than 3 characters)
+			// replace prepositions (greater than 3 characters)
 				elehtml = elehtml.replace(preps, function(contents, p1, p2) {
-				        return p1 + p2.replace(/\s/gi, '&#160;');
-				    });
+					return p1 + p2;
+				});
 
 			if (method == 'small-words' || method == 'all')
 
-				// replace small words
+			// replace small words
 				elehtml = elehtml.replace(smallwords, function(contents, p1, p2) {
-		        return p1 + p2.replace(/\s/g, '&#160;');
-		    });
+					return p1 + p2;
+				});
 
-		  if (method == 'dashes' || method == 'all')
+			if (method == 'dashes' || method == 'all')
 
-		  	// replace small words
-		  	elehtml = elehtml.replace(dashes, function(contents) {
-		        return contents.replace(/\s/g, '&#160;');
-		    });
+			// replace small words
+				elehtml = elehtml.replace(dashes, function(contents) {
+					return contents;
+				});
 
 			if (method == 'emphasis' || method == 'all')
 
-				// emphasized text
+			// emphasized text
 				elehtml = elehtml.replace(emphasis, function(contents, p1, p2, p3, p4, p5) {
-				        return p1 + p3.replace(/\s/gi, '&#160;') + p5;
-				    });
+					return p1 + p3 + p5;
+				});
 
 			ele.innerHTML = elehtml;
 
