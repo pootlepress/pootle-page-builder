b3508c2857f8c058c01444a9eed9e2689b9bf805
Shramee Srivastav <shramee.srivastav@gmail.com>
2016-05-06 11:21:46 +0530

    Adding 4 nice templates :smiley:

--- ppbpro/inc/tpl-templates.php
+++ ppbpro/inc/tpl-templates.php
@@ -8,15 +8,44 @@
 
 global $ppbpro_tpl;
 
-$plg_url = Pootle_Page_Builder_Pro::$url . 'assets/tpl';
+$plg_url = Pootle_Page_Builder_Pro::$url . 'assets/tpl/thumbs';
 $live_page_url = admin_url( 'admin-ajax.php?action=pootlepb_live_page' );
 $live_page_url = wp_nonce_url( $live_page_url, 'ppb-new-live-post', 'ppbLiveEditor' );
 ?>
+<style>
+	.ppb-templates {
+		text-align: center;
+	}
+
+	.ppb-template {
+		display: inline-block;
+		width: 45%;
+		max-width: 500px;
+		min-width: 300px;
+		margin: 2.5%;
+		font-weight: 200;
+		font-size: 25px;
+		text-decoration: none;
+		color: #555;
+		text-align: center;
+
+	}
+	.ppb-template img {
+		display: block;
+		width: 100%;
+		height: auto;
+		float: right;
+		margin: 0 0 25px;
+	}
+</style>
 <div class="wrap">
 	<h2>Starter Templates</h2>
-	<?php
-	foreach ( ppbpro_get_template() as $tpl ) {
-		echo "<a href='$live_page_url&tpl=$tpl'><img src='$plg_url/$tpl.jpg'></a>";
-	}
-	?>
+	<div class="ppb-templates">
+		<?php
+		foreach ( ppbpro_get_template() as $Tpl ) {
+			$tpl = strtolower( $Tpl );
+			echo "<a class='ppb-template' href='$live_page_url&tpl=$Tpl'><img src='$plg_url/$tpl.png'>$Tpl</a>";
+		}
+		?>
+	</div>
 </div>
