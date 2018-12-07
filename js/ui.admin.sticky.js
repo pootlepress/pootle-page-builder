d6c234a1b740c6203ff07e03b7025730b0509de8
Shramee Srivastav <shramee.srivastav@gmail.com>
2018-12-07 21:36:04 +0530

    Addressing #742

--- js/ui.admin.sticky.js
+++ js/ui.admin.sticky.js
@@ -2,49 +2,51 @@
  * Created by shramee on 11/5/15.
  * @since 0.1.0
  */
-jQuery(document).ready(function ($) {
+jQuery( document ).ready( function ( $ ) {
 
-    //Our fixitigation element caching for better performance
-    var fixit = $('#wp-content-editor-tools'),
-        container = $('#postdivrich'),
-        panelsBar = $('#pootlepb-panels .pootlepb-toolbar');
+	//Our fixitigation element caching for better performance
+	var fixit     = $( '#wp-content-editor-tools' ),
+			container = $( '#postdivrich' ),
+			panelsBar = $( '#pootlepb-panels .pootlepb-toolbar' );
+	if ( ! fixit.length ) {
+		return;
+	}
+	//fixit offset cached since it will change later
+	var stickyFixitTop = fixit.offset().top;
 
-    //fixit offset cached since it will change later
-    var stickyFixitTop = fixit.offset().top;
+	panelsBar.css( 'width', container.width() - 2 );
 
-    panelsBar.css( 'width', container.width()-2);
+	//stickyfixit function
+	function stickyfixit() {
 
-    //stickyfixit function
-    function stickyfixit() {
+		if ( ! fixit.parent().hasClass( 'panels-active' ) ) {
+			container.removeClass( 'sticky' );
+			return;
+		}
 
-        if (!fixit.parent().hasClass('panels-active')) {
-            container.removeClass('sticky');
-            return;
-        }
+		panelsBar.outerWidth( container.width() - 2 );
 
-        panelsBar.outerWidth(container.width() - 2);
+		//Current Scroll Position
+		var scrollTop = $( window ).scrollTop();
 
-        //Current Scroll Position
-        var scrollTop = $(window).scrollTop();
+		if ( scrollTop > stickyFixitTop - 22 && $( document ).width() > 768 ) {
+			//Adding class for distributing styles to stylesheet
+			container.addClass( 'sticky' );
 
-        if (scrollTop > stickyFixitTop - 22 && $(document).width() > 768) {
-            //Adding class for distributing styles to stylesheet
-            container.addClass('sticky');
+		} else {
+			//Removing CSS class
+			container.removeClass( 'sticky' );
+		}
+	};
 
-        } else {
-            //Removing CSS class
-            container.removeClass('sticky');
-        }
-    };
+	//On runtime if page is already scrolled down
+	stickyfixit();
 
-    //On runtime if page is already scrolled down
-    stickyfixit();
-
-    //For every scroll
-    $(window).scroll(function () {
-        stickyfixit();
-    });
-    $(window).resize(function(){
-        panelsBar.css( 'width', container.width()-2);
-    });
-});
+	//For every scroll
+	$( window ).scroll( function () {
+		stickyfixit();
+	} );
+	$( window ).resize( function () {
+		panelsBar.css( 'width', container.width() - 2 );
+	} );
+} );
